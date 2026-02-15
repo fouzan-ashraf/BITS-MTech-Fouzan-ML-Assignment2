47d7b419d661408eda00b139ee7534f42d34e822
fouzan-ashraf <fouzan23@gmail.com>
2026-02-15 04:00:23 -0500

    updated text boldness

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ with tab1:
     st.markdown("""
     **Dataset Details:**
     This dataset consists of 30 total features (excluding target variable "diagnosis" and non-predictive column like "id") computed from a digitized image of a fine needle aspirate (FNA) of a breast mass. These features mathematically describe the characteristics of the cell nuclei present in the image, capturing 10 distinct traits (such as radius, texture, and area) across their mean, standard error, and 'worst' (largest) values.
-    * **Dataset Source: Kaggle (https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data)
+    * **Dataset Source:** Kaggle (https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data)
     * **Target Variable:** `diagnosis`
     * **Target Classes:** `M` = Malignant (Cancerous), `B` = Benign (Non-Cancerous)
     * **Key Features:** Radius, Texture, Perimeter, Area, Smoothness, Compactness, Concavity, Symmetry, and Fractal Dimension.
